e56a8db8059d316a90b7a88c8271acc77978dfa0
Deven <devenzachuth@gmail.com>
2022-04-12 09:35:50 -0500

    commit tree e1c4e04d92bff1bd22cfb8cb14111a751e1dc279

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    const p = document.querySelector('#text');
+    console.log(p)
+    p.innerHTML = "This is really cool!"
+  });
